0644a8190de1489d373eb6ebe95a34e2b6becfc7
tachyon <83tachyon@gmail.com>
2021-01-05 11:18:56 +0900

    morgan and superuser

--- dbSetup_local.js
+++ dbSetup_local.js
@@ -1,6 +1,8 @@
 const mysql = require('mysql');
 const dbSetting = require('./models/settings/dbConnectionSettings')
 const sqls = require('./models/settings/sqlDispenser')
+const bcrypt = require('bcrypt')
+const saltRounds = 10
 
 let rootSettingObj = {
     host: dbSetting.host,
@@ -16,6 +18,18 @@ let settingObj = {
     password: dbSetting.password,
     multipleStatements: true,
 }
+
+const sql_addSuperuser =
+    `insert into user(name,email,password,conversionid,help,admin) 
+    values(?,?,?,?,?,?);`
+
+// maybe... need to make conversionId nullable?
+let superuser = [
+    'superuser',
+    'superuser@gpa.com',
+    'abcd1234',
+    1, 0, 1,
+]
 
 function db_initSetting() {
     return new Promise((resolve, reject) => {
@@ -33,9 +47,23 @@ function db_initSetting() {
                 const conn_init3 = mysql.createConnection(settingObj)
                 conn_init3.connect()
                 conn_init3.query(sqls.createDummy, (err) => {
-                    conn_init3.destroy()
-                    if (err) reject(err)
-                    resolve();
+                    if (err) {
+                        conn_init3.destroy()
+                        reject(err)
+                    }
+                    bcrypt.genSalt(saltRounds)
+                        .then(salt => {
+                            return bcrypt.hash(superuser[2], salt)
+                        })
+                        .then(hash => {
+                            superuser[2] = hash
+                            conn_init3.query(sql_addSuperuser, superuser, err => {
+                                conn_init3.destroy();
+                                if (err) reject(err);
+                                resolve();
+                            })
+                        })
+                        .catch(err => reject(err))
                 })
             })
         })
